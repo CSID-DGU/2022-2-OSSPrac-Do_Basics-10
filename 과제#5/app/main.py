9b5f85c7041137ddbb9e9f804397ab0e63fd7f0b
hooooooni <kheee2010@dgu.ac.kr>
2022-11-16 21:25:02 +0900

    feat: Add 과제#5

---
+++ 과제#5/app/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')  # default URL
+def student():
+   return render_template('input_info.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = dict()
+      result['Name'] = request.form.get('Name')
+      result['Student Number'] = request.form.get('Student Number') # 학번
+      result['Gender'] = request.form.get('Gender') #성별
+      # 학과
+      result['Programming Languages'] = ','.join(request.form.getlist("PL_list")) #프로그래밍언어
+
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run()
